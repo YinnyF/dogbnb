31a32cb27dac1345601eb5f319333b90084e41b1
Zsuzsanna Ver <56695526+MrsVer@users.noreply.github.com>
2021-06-29 12:40:32 +0100

    property details form

---
+++ spec/features/providing_decription_for_space_spec.rb
@@ -0,0 +1,9 @@
+feature 'Providing description' do
+  scenario 'User can add a description and price' do
+    visit '/property/new'
+    fill_in 'description', with: 'This is a nice place.'
+    fill_in 'price', with: 100
+    expect(page).to have_content 'This is a nice place.'
+    expect(page).to have_content '100'
+  end
+end
