a3d742e47641ccb179d1083a961b2b05981e8d1b
JooMin Choi <joominchoi23@gmail.com>
2021-06-29 16:32:45 +0100

    rebase testing

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
+require './lib/listing'
 
 class DogBnB < Sinatra::Base
   configure :development do
@@ -9,6 +10,20 @@ class DogBnB < Sinatra::Base
   get '/' do
     'Hello DogBnB!'
   end
+
+  get '/listings' do
+    @listings = Listing.all
+    erb :'listings/index'
+  end
+
+  get '/listings/new' do
+    erb :'listings/new'
+  end
+
+  # post '/listings' do
+  #   Listing.create(name: params[:name])
+  #   redirect '/listings'
+  # end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
