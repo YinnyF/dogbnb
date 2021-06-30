0976c88295a8e9888f6506e3f9bb90143ccefce9
Sean Phillips <seanphillips@Seans-Air.broadband>
2021-06-30 12:08:31 +0100

    error for wrong email

--- lib/user.rb
+++ lib/user.rb
@@ -25,7 +25,8 @@ class User
 
   def self.authenticate(email:, password:)
     result = DatabaseConnection.query("SELECT * FROM users WHERE email = '#{email}'")
-    User.new(id: result[0]['id'], name: result[0]['name'], email: result[0]['email'])
+      return unless result.any?
+      User.new(id: result[0]['id'], name: result[0]['name'], email: result[0]['email'])
   end
   
 end
