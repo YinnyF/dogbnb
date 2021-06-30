405ef267953e934bbfaa2dea230c7a5046f16b0a
Sean Phillips <seanphillips@Seans-Air.broadband>
2021-06-30 14:42:46 +0100

    adding failure for wrong password

--- spec/features/authentication_spec.rb
+++ spec/features/authentication_spec.rb
@@ -9,15 +9,25 @@ feature 'authentication' do
 
     expect(page).to have_content 'Welcome, Testname'
   end
-end
 
-feature 'authentication' do
   scenario 'a user sees an error if they get their email wrong' do
     User.create(name: 'Testname', email: 'test@example.com', password: 'password123')
 
     visit '/sessions/new'
     fill_in(:email, with: 'nottherightemail@me.com')
     fill_in(:password, with: 'password123')
+    click_button('Sign in')
+
+    expect(page).not_to have_content 'Welcome, Testname'
+    expect(page).to have_content 'Please check your email or password.'
+  end
+
+  scenario 'a user sees an error if they get their password wrong' do
+    User.create(name: 'Testname', email: 'test@example.com', password: 'password123')
+
+    visit '/sessions/new'
+    fill_in(:email, with: 'test@example.com')
+    fill_in(:password, with: 'wrongpassword')
     click_button('Sign in')
 
     expect(page).not_to have_content 'Welcome, Testname'
