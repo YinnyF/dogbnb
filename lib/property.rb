b45191f883dc2dd6cd80814e86e7a661833e3981
Masako Gray <masakogray@hotmail.co.uk>
2021-06-30 15:12:51 +0100

    conflicts resolved, migrated name into views file

--- lib/property.rb
+++ lib/property.rb
@@ -20,7 +20,7 @@ class Property
     result = DatabaseConnection.query("INSERT INTO properties (name, description, price) VALUES('#{name}', '#{description}','#{price}') RETURNING id, name, description, price;")
     Property.new(
       id: result[0]['id'],
-      name: property[0]['name'],
+      name: result[0]['name'],
       description: result[0]['description'],
       price: result[0]['price']
     )
--- spec/features/viewing_listings_spec.rb
+++
@@ -1,7 +0,0 @@
-feature 'Viewing listings' do
-  scenario 'Visiting listing page' do
-    visit '/listings'
-    expect(page).to have_content("Maru's Crib")
-    expect(page).to have_content("Maru's Garage")
-  end
-end
