e323a46e5cc115035277df8efc6e0adc11627b75
Masako Gray <masakogray@hotmail.co.uk>
2021-07-02 00:52:56 +0100

    removed duplicates

--- db/migrations/2_create_properties_table.sql
+++ db/migrations/2_create_properties_table.sql
@@ -1,1 +1,3 @@
 CREATE TABLE properties (id SERIAL PRIMARY KEY, name VARCHAR(100) NOT NULL, description VARCHAR(200) NOT NULL, price NUMERIC(10, 2) NOT NULL);
+ALTER TABLE properties ADD COLUMN available_from DATE;
+ALTER TABLE properties ADD COLUMN available_to DATE;
--- db/migrations/3_adding_foreign_key_for_ownerID.sql.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE properties ADD COLUMN available_from DATE;
-ALTER TABLE properties ADD COLUMN available_to DATE;
-ALTER TABLE properties ADD owner_id int;
-ALTER TABLE properties ADD FOREIGN KEY (owner_id) REFERENCES users(id);
